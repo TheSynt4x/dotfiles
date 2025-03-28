e72f8ea77cdabb21bccf0a9f8f1e18933f3a2430
TheSynt4x <anonymousehaxor@gmail.com>
2025-03-28 13:40:09 +0100

    fix things

--- copy.sh
+++ copy.sh
@@ -1,16 +1,8 @@
-#!/bin/bash
+tar --exclude='nixos/configuration.nix' \
+    --exclude='nixos/hardware-configuration.nix' \
+    -czf ~/dotfiles/nixos.tar.gz -C /etc nixos
 
-# Script to copy NixOS config files to dotfiles repo while preserving directory structure
-# Excludes configuration.nix and hardware-configuration.nix
+tar -xzf ~/dotfiles/nixos.tar.gz -C ~/dotfiles/
 
-echo "Copying NixOS config files to dotfiles repo..."
-
-# Create the target directory if it doesn't exist
-mkdir -p ~/dotfiles/etc/nixos
-
-# Use rsync to copy files while excluding specific ones
-rsync -av --exclude='configuration.nix' \
-         --exclude='hardware-configuration.nix' \
-         /etc/nixos ~/dotfiles/
-
-echo "Done! NixOS config files copied from /etc/nixos to ~/dotfiles/nixos"
+# remove the tar.gz file
+rm ~/dotfiles/nixos.tar.gz
