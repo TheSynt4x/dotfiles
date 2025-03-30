9f3b59edf356cac9ccba2ed98cd4aebc41a89bd7
TheSynt4x <anonymousehaxor@gmail.com>
2025-03-30 14:44:19 +0200

    fix lockscreen

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -202,13 +202,13 @@ in
               "$mainMod, R, exec, wofi --show drun"
               ", Print, exec, grimblast copy area"
 
-              # Workspace switching
+              # Window navigation
               "$mainMod, H, movefocus, l"
               "$mainMod, L, movefocus, r"
               "$mainMod, K, movefocus, u"
               "$mainMod, J, movefocus, d"
 
-              "$mainMod, L, exec, swaylock"
+              "SUPER, L, exec, swaylock"
 
               "$mainMod, S, togglespecialworkspace, slack"
               "$mainMod, D, togglespecialworkspace, dev"
