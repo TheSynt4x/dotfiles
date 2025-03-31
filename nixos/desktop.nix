8870ac5681eefc763dd33fa4e526e94b19e0e939
TheSynt4x <anonymousehaxor@gmail.com>
2025-03-31 09:44:13 +0200

    some fixes

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -35,9 +35,20 @@ in
     enable = true;
 
     settings = {
-      # CPU scaling governor
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+      RUNTIME_PM_ON_AC = "auto";
+      RUNTIME_PM_ON_BAT = "auto";
     };
   };
 
--- wayland/waybar/scripts/power-menu.sh
+++ wayland/waybar/scripts/power-menu.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+entries="Suspend\n⭮ Reboot\n⏻ Shutdown"
 
 selected=$(echo -e $entries | wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
-  logout)
-    hyprctl dispatch exit;;
   suspend)
     systemctl suspend;;
   reboot)
